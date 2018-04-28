6b3c26fa0fd9ca87bf1fb92d246e22e85c82c411
willelz <37827548+willelz@users.noreply.github.com>
2018-04-28 23:00:33 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,19 +2,11 @@ set PATH $HOME/.nodebrew/current/bin $PATH
 alias vim="nvim"
 alias la="ls -pA"
 
-##################################################
-# git
+#エラー回避
+function x86
+  echo '(x86)'
+end
 
-set git_dirty_color red
-set git_not_dirty_color green
-
-function parse_git_branch
-  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
-  set -l git_diff (git diff)
-
-  if test -n "$git_diff"
-    echo (set_color $git_dirty_color)$branch(set_color normal)
-  else
-    echo (set_color $git_not_dirty_color)$branch(set_color normal)
-  end
+function R
+  echo 'R'
 end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,18 @@
+set git_dirty_color red
+set git_not_dirty_color green
+
+function parse_git_branch
+  set -l branch (git branch 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+set -l git_diff (git diff)
+
+  if test -n "$git_diff"
+    echo (set_color $git_dirty_color)$branch(set_color normal)
+  else
+    echo (set_color $git_not_dirty_color)$branch(set_color normal)
+  end
+end
+
+
 function fish_prompt
   if [ $status -eq 0 ]
     set status_face (set_color green)"(*'-') < "
